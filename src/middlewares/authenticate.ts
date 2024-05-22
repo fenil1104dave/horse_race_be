1bee1621cda8c807f3e4a41f73a1495c66f8a381
Fenil Dave <fenil1104dave@gmail.com>
2024-05-22 18:09:03 +0530

    Updated response structure of authenticate middleware.

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -2,15 +2,16 @@ import { User } from "contracts/user/types";
 import { Request, Response, NextFunction } from "express";
 import passport from "passport";
 import { unless } from "express-unless";
+import { errorResponse } from "../utils/responseUtils";
 
 // Middleware to protect routes
 const authenticate = (req: Request, res: Response, next: NextFunction) => {
   passport.authenticate("jwt", { session: false }, (err: Error, user: User) => {
     if (err) {
-      return res.status(500).json({ error: "Internal Server Error" });
+      return res.status(500).json(errorResponse("Internal Server Error", err));
     }
     if (!user) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json(errorResponse("Unauthorized"));
     }
     req.user = user;
     next();
