78f43d50abc666d14cb2b61ef776acf3eb938b66
Fenil Dave <fenil1104dave@gmail.com>
2024-05-24 16:05:42 +0530

    Added websocket config.

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 import passport from "passport";
+import { Server, Socket } from "socket.io";
+import { createServer } from "node:http";
 
 import authenticate from "./middlewares/authenticate";
 
@@ -13,10 +15,32 @@ dotenv.config();
 
 const app = express();
 
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // React app's URL
+  },
+});
+
+// Socket.IO setup
+io.on("connection", (socket: Socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("message", (msg: string) => {
+    console.log("Message received:", msg);
+    io.emit("message", msg + "From BE"); // Broadcast the message to all clients
+  });
+});
+
+// Middleware setup
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(cors());
 
 app.use(passport.initialize());
 require("./config/passport");
@@ -30,8 +54,10 @@ app.use(
   })
 );
 
+// Database connection
 connectDB();
 
+// Initialize routes
 initRouters(app);
 
-export default app;
+export default server;
